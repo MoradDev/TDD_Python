3d970dae2f3f743a298c98a3950ed6010dfc0185
MoradDev <morad.mlik@protonmail.com>
2023-09-22 11:53:10 +0200

    premier fail test et if name = main

--- test_ma_class.py
+++ test_ma_class.py
@@ -1,6 +1,17 @@
 import unittest
+from ma_class import *
+
+sample=MaClass("premiere_class")
 
 class MonTest(unittest.TestCase):
     
     def setUp(self):
         pass
+
+    def test_aire_cercle(self):
+        with self.subTest:
+            self.assertEqual(sample.aire_cercle(0), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
